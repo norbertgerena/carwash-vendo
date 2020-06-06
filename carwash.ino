a02ec9c46a14e94d9ce79db8f3848e44af57803f
norbertgerena <norbertgerena.msds2021@aim.edu>
2020-06-06 16:14:56 +0800

    fixed isr

--- carwash.ino
+++ carwash.ino
@@ -9,7 +9,7 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 //pins to use
 const int coinPin = 2;
-const int button1 = 3, button2 = 4, button3 = 5;
+const int button1 = 8, button2 = A0, button3 = 4;
 const int relay1 = 10, relay2 = 11, relay3 = 12;
 const int button1pushed = 0b00110000;
 const int button2pushed = 0b00101000;
@@ -19,16 +19,17 @@ volatile long unsigned eventTime;
 volatile long unsigned previousEventTime;
 volatile long unsigned timeSinceLastEvent;
 volatile byte portDstatus;
-volatile byte eventFlag;
+volatile byte eventFlag1, eventFlag2, eventFlag3;
 //additonal variables
 unsigned long timeSpent = 0, startTime = 0;
 volatile long unsigned timeLeft = 0;
 int coinTimeVal = 5;  //how much time per coin
 int event;
+bool event1running = false, event2running = false, event3running =false;
 
 void setup(){
   
-  //Serial.begin(9600);//start serial communication
+  Serial.begin(9600);//start serial communication
   
   //define the pins:
   pinMode(button1, INPUT);//define pin as INPUT  
@@ -45,11 +46,12 @@ void setup(){
   digitalWrite(relay3, HIGH);
   
   //setting up the interrupt: (see chapter 12 in the ATmega328p data sheet for an explanation which register does what etc...)
-  sbi (PCICR,PCIE2);//enable interrupt PCI2
-  sbi (PCMSK2,PCINT21); //button3
-  sbi (PCMSK2,PCINT20); //button2
-  sbi (PCMSK2,PCINT19); //button1
-
+  cli();
+  PCICR |= 0b00000111;    // turn on all ports
+  PCMSK0 |= 0b00000001;    // turn on pin PB0, which is PCINT0, pin 8 
+  PCMSK1 |= 0b00000001;    // turn on pin PC4, which is PCINT12, pin A0
+  PCMSK2 |= 0b00010000;    // turn on pin 4
+  sei();
   //lcd init
   lcd.begin(16, 2);
 
@@ -60,58 +62,110 @@ void setup(){
 
 void loop()
 {
-  if (timeLeft){
+  if (timeLeft <= 0){
+    if (timeLeft < 0){timeLeft=0;}
     welcomeInsertCoinMessage();
+    if (event1running){
+      switchRelay(relay1, event1running);
+      event1running ^= event1running;  
     }
+    else if (event2running){
+      switchRelay(relay2, event2running);
+      event2running ^= event2running;  
+    }
+    else if (event3running){
+      switchRelay(relay3, event3running);
+      event3running ^= event3running;  
+    }
+  }
   else {
-    if (eventFlag == 1 && (eventTime-previousEventTime>50)){//button event causes eventFlag to be set in ISR. We only consider events occurring 50ms after the last one.
-                                                           //This debounces the button.
-                                                           
+    printTime(timeLeft);
+    if (eventFlag1 == 1 && (eventTime-previousEventTime>100)){
       previousEventTime=eventTime;//note the present event time as last time for the next event
       startTime = seconds();
-      event = (portDstatus|0b00111000);
-      switch(event){
-        case button1pushed:
-          break;
-        case button2pushed:
-          break;
-        case button3pushed:
-          break; 
-        }
-      eventFlag = 0;//reset the event flag for the next event
+      switchRelay(relay1, event1running);
+      event1running ^= event1running;
+      if (event1running){
+        printStatus("Wash...");  
+      }else{
+        printStatus("Washing paused"); 
+      }
+      eventFlag1 = 0;//reset the event flag for the next event
+    }
+    else if (eventFlag2 == 1 && (eventTime-previousEventTime>100)){
+      previousEventTime=eventTime;//note the present event time as last time for the next event
+      startTime = seconds();
+      switchRelay(relay2, event2running);
+      event2running ^= event2running;
+      if (event2running){
+        printStatus("Wash...");  
+      }else{
+        printStatus("Washing paused"); 
+      }
+      eventFlag2 = 0;//reset the event flag for the next event
+    }
+    else if (eventFlag3 == 1 && (eventTime-previousEventTime>100)){
+      previousEventTime=eventTime;//note the present event time as last time for the next event
+      startTime = seconds();
+      switchRelay(relay3, event3running);
+      event3running ^= event3running;
+      if (event3running){
+        printStatus("Wash...");  
+      }else{
+        printStatus("Washing paused"); 
+      }
+      eventFlag3 = 0;//reset the event flag for the next event
+    }
+    if (event1running || event2running || event3running){
+      timeSpent = seconds() - startTime;
+      timeLeft -= timeSpent;
+    } else{
+      startTime = seconds();
     }
   }
 }
 
 
 
-ISR (PCINT2_vect)//Interrupt Service Routine. This code is executed whenever a button event (either pressing or releasing) occurs.
-{
-  
-  portDstatus=PIND;//PIND is the register that contains the momentary status of the pins of Port D (pins 0-7)
-                   
+ISR (PCINT0_vect){
   eventTime=millis();//note the time of the ISR call  
-  eventFlag=1;//set the event flag that tells the main loop that a button was pressed.
+  eventFlag1=1;//set the event flag that tells the main loop that a button was pressed.
 }
 
+ISR (PCINT1_vect){
+  eventTime=millis();//note the time of the ISR call  
+  eventFlag2=1;//set the event flag that tells the main loop that a button was pressed.
+}
+
+ISR (PCINT3_vect){
+  eventTime=millis();//note the time of the ISR call  
+  eventFlag3=1;//set the event flag that tells the main loop that a button was pressed.
+}
 void coinInterrupt(){
   timeLeft += coinTimeVal; 
 }
 
 void welcomeInsertCoinMessage(){
-    lcd.clear();
     lcd.setCursor(4, 0); //Start at character 4 on line 0
     lcd.print("WELCOME!");
-    delay(100);
     lcd.setCursor(0, 1);
     lcd.print(" ->Insert Coin<-");
+    lcd.clear();
   }
 
-void printStatusAndTime(String timer, String statusAction){
-    lcd.clear();
+void printTime(int timer){
+    //lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(timer);
-    delay(100);
-    lcd.setCursor(0, 1);
-    lcd.print(statusAction);
+//    delay(100);
+//    lcd.setCursor(0, 1);
+//    lcd.print(statusAction); 
+}
+
+void printStatus(String s){
+  lcd.setCursor(0, 1);
+  lcd.print(s);
+}
+void switchRelay(int relaynum, bool onOff){
+  digitalWrite(relaynum, onOff);
   }
