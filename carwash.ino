69ed9663914aae4f72db1e9a73fbee9e5acd49b0
norbertgerena <norbertgerena.msds2021@aim.edu>
2020-06-29 00:28:43 +0800

    fixed display and added coin slot

--- carwash.ino
+++ carwash.ino
@@ -19,11 +19,11 @@ volatile long unsigned eventTime;
 volatile long unsigned previousEventTime;
 volatile long unsigned timeSinceLastEvent;
 volatile byte portDstatus;
-volatile byte eventFlag1, eventFlag2, eventFlag3;
+volatile byte eventFlag0, eventFlag1, eventFlag2, eventFlag3;
 //additonal variables
-unsigned long timeSpent = 0, startTime = 0, coinTimeVal = 60 * 1000;
-volatile long unsigned timeLeft = 0;
-int event;
+long timeSpent = 0, startTime = 0, coinTimeVal = 10000;
+long timeLeft = 0;
+int event, pulse = 0;
 bool event1running = false, event2running = false, event3running =false;
 
 void setup(){
@@ -62,6 +62,14 @@ void setup(){
 
 void loop()
 {
+//  if (eventFlag0 == 1){ //&& (eventTime-previousEventTime<100)){
+//      previousEventTime=eventTime;
+//      pulse += 1;
+//      eventFlag0 = 0;
+//      if (not(pulse % 6)){
+//        timeLeft += coinTimeVal;
+//        pulse = 0;
+//        }
   if (timeLeft <= 0){
     if (timeLeft < 0){timeLeft=0;}
     welcomeInsertCoinMessage();
@@ -80,6 +88,7 @@ void loop()
   }
   else {
     printTime(timeLeft);
+    Serial.println(int(timeLeft/1000));
     if (eventFlag1 == 1 && (eventTime-previousEventTime>100)){
       previousEventTime=eventTime;//note the present event time as last time for the next event
       switchRelay(relay1, event1running);
@@ -119,7 +128,7 @@ void loop()
     }
     if (event1running || event2running || event3running){
       //Serial.println("running...");
-      timeLeft -= millis() - startTime;
+      timeLeft -= (millis() - startTime);
       startTime = millis();
     }
   }delay(100);
@@ -146,7 +155,9 @@ void button3event(){
   eventFlag3=1;//set the event flag that tells the main loop that a button was pressed.
 }
 void coinInterrupt(){
-  timeLeft += coinTimeVal; 
+  timeLeft += coinTimeVal;
+//  eventTime=millis();
+//  eventFlag0 = 1; 
 }
 
 void welcomeInsertCoinMessage(){
@@ -158,14 +169,17 @@ void welcomeInsertCoinMessage(){
     
   }
 
-void printTime(int timer){
-    int mins, secs;
+void printTime(long timer){
+    long mins, secs, real_sec;
     String t;
     //lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print("               ");
-    mins =int(timer/(60*1000));
-    secs = int((timer%(60*1000))/1000);
+    real_sec = timer/1000;
+    //mins =(long)(timer/(60*1000));
+    mins = real_sec/60;
+    secs = real_sec%60;
+    Serial.println(secs, mins);
     if (mins > 9 and secs >9){
       t = String("Time  ") + String(mins) + String(":") + String(secs);
     }else if (mins > 9 and secs < 10){
